e28f57cf8f8aee419e3a55a055387ab344ce0265
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-07-09 18:47:15 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Welcome to the frontend part</h1>
+      <button className="btn btn-primary">Primary</button>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>This is main layout visuals</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
